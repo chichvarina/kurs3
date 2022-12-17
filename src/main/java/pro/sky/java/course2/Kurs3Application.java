77b96eb8158f4c51dcfc88c9b6d6c8b355d3e46c
MonakhovA <MonGraph67>
2022-12-17 06:29:44 +0300

    commit

---
+++ src/main/java/pro/sky/java/course2/Kurs3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs3Application.class, args);
+    }
+
+}
